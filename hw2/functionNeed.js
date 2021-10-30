44cf8d09b193aa69051e980b717d3799c7fe8771
weslia <abc12340201@gmail.com>
2021-10-30 22:45:35 +0800

    hw2-6

--- hw2/functionNeed.js
+++ hw2/functionNeed.js
@@ -71,55 +71,5 @@ export function buildVehicle() {
     return car;
 }
 
-export function makeRect() {
-    let Rect = {};
-    Rect.max = new THREE.Vector3(8, 0, 5);
-    Rect.min = new THREE.Vector3(-8, 0, -5);
-    return Rect;
-}	
 
-export function Check_Intersect(Rect, C, Rad) {
-    const Rad2 = Rad * Rad;
-
-    /* Translate coordinates, placing C at the origin. */
-    // duplicate R object here for computation ...
-    let R = objectCopy (Rect);
-    let c = car.worldToLocal(C.clone().position);
-
-    R.max.x -= c.x;  R.max.z -= c.z;
-    R.min.x -= c.x;  R.min.z -= c.z;
-
-    if (R.max.x < 0) 			/* R to left of circle center */
-        if (R.max.z < 0) 		/* R in lower left corner */
-            return ((R.max.x * R.max.x + R.max.z * R.max.z) < Rad2);
-        else if (R.min.z > 0) 	/* R in upper left corner */
-            return ((R.max.x * R.max.x + R.min.z * R.min.z) < Rad2);
-        else 					/* R due West of circle */
-            return(Math.abs(R.max.x) < Rad);
-    else if (R.min.x > 0)  	/* R to right of circle center */
-            if (R.max.z < 0) 	/* R in lower right corner */
-                return ((R.min.x * R.min.x + R.max.z * R.max.z) < Rad2);
-        else if (R.min.z > 0)  	/* R in upper right corner */
-            return ((R.min.x * R.min.x + R.min.z * R.min.z) < Rad2);
-        else 				/* R due East of circle */
-            return (R.min.x < Rad);
-    else				/* R on circle vertical centerline */
-            if (R.max.z < 0) 	/* R due South of circle */
-            return (Math.abs(R.max.z) < Rad);
-        else if (R.min.z > 0)  	/* R due North of circle */
-            return (R.min.z < Rad);
-        else 				/* R contains circle centerpoint */
-            return(true);
-} 
-
-export function traverse(pos) {
-    if(pos.x > 100)
-        pos.x = -100;
-    if(pos.x < -100)
-        pos.x = 100;
-    if(pos.z > 100)
-        pos.z = -100;
-    if(pos.z < -100)
-        pos.z = 100;
-}
 
